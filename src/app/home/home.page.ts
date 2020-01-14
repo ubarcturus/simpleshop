265e621066bb291069a7b8e0b14fc1e0f7d9006c
ubarcturus <ubarcturus@gmail.com>
2020-01-14 21:42:16 +0100

    initial

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Server } from 'http';
+import { networkInterfaces } from 'os';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor() {}
+
+}
